d813a4287dc6fbab100878dbc01c8f22eaf71b5c
phaniso <9phaniso9@gmail.com>
2016-02-29 06:28:06 +0100

    Add countries example

---
+++ examples/Countries/Countries.php
@@ -0,0 +1,25 @@
+<?php
+header('Content-Type: application/json');
+
+use Autocomplete\Factory\ContainerFactory;
+
+require_once(__DIR__.'/../bootstrap.php');
+
+$containerFactory = new ContainerFactory;
+$trie = $containerFactory->build('Trie', [false]);
+
+function loadWords(&$trie) {
+    $file = new SplFileObject(__DIR__."/countries.txt");
+    while ( ! $file->eof()) {
+        $country = trim($file->fgets());
+        $country = explode("|", $country)[1];
+        $trie->addWord($country);
+    }
+}
+
+loadWords($trie);
+
+$countryInput = $_GET['key'];
+$countries = $trie->getByPrefix($countryInput);
+
+echo json_encode($countries);
